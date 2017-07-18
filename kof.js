5b4834c98c10132f89e7f0c799d024d1b7189b45
jack andrews <effbiae@ivorykite.com>
2017-07-18 12:03:00 +1000

    x

--- kof.js
+++ kof.js
@@ -1,4 +1,4 @@
-var kof,wscon,wsset,wsget,wsclo;
+var ocon,opub,wscon,wsset,wsget,wsclo;
 (function(){ "use strict";
   function L(x){console.log(x);}
   wsset=function(w,x,f,g){w.onmessage=f;w.onerror=g;return w.send(x);};
@@ -9,7 +9,7 @@ var kof,wscon,wsset,wsget,wsclo;
   wsget=function(w,x){return new Promise((f,g)=>{wsset(w,x,f,g)});};
   wsclo=function(w){w.close();}
  }
- kofcon=function(x)
+ ocon=function(x)
  {if("fin"       in window) openfin();
   else wscon(L);
  }
